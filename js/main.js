0624e2b7cda65731963bd8d33369668a12eef5a1
Le Deng <lede@Les-MacBook-Pro.local>
2016-07-13 16:08:40 -0400

    third commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+    // Plugin initialization
+    $('.slider').slider({
+    });
+    $('.carousel').carousel({
+      dist:-300,
+    });
+
+    setInterval(function(){$('.carousel').carousel('next')}, 10000);
+})
+
 var likes=0;
 
 function increment() {
@@ -5,8 +16,16 @@ function increment() {
   console.log(likes);
   $(".like-display").html(likes);
   // Materialize.toast(message, displayLength, className, completeCallback);
- Materialize.toast('Cool! Thank you!', 4000, 'rounded') // 4000 is the duration of the toast
+ Materialize.toast('Cool. Thank you!', 4000, 'rounded') // 4000 is the duration of the toast
 
+}
+
+function carouselLeft() {
+  $('.carousel').carousel('prev');
+}
+
+function carouselRight() {
+  $('.carousel').carousel('next');
 }
 
 $(".like-display").html(likes);
